2665efe38addea00ae076c940f4c80db98f1bb72
Muhammad Farabie <farabie024@gmail.com>
2023-11-07 09:38:28 +0700

    Composer Update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'abieproject/belajar-php-composer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c755576e585ef616672733b1a912a23ffa2606a3',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'abieproject/belajar-php-composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c755576e585ef616672733b1a912a23ffa2606a3',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
